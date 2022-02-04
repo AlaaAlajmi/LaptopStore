8a312f8555a72e244de85f861dcfc23e860f9b27
AlaaAlajmi <9223@kfh.com>
2022-02-04 22:33:58 +0300

    Done

--- LaptopStore/Data/AppDbInitializer.cs
+++ LaptopStore/Data/AppDbInitializer.cs
@@ -24,7 +24,7 @@ namespace LaptopStore.Data
 						{
 							ProductName = "Huawei Matebook D14",
 							Description ="Intel Core i5 10th Gen, RAM 8GB, 512 SSD, 14-inch Laptop",
-							Price =  219.900 ,
+							Price =  219 ,
 							ImageUrl = "https://m.xcite.com/media/catalog/product/cache/1/thumbnail/700x700/9df78eab33525d08d6e5fb8d27136e95/h/u/huawei-matebook-d14-laptop-grey_1.jpg"
 
 						},
@@ -32,21 +32,21 @@ namespace LaptopStore.Data
 						{
 							ProductName = "Lenovo V14",
 							Description ="Intel Core i3, 4GB RAM, 1TB HDD, 14-inch Laptop",
-							Price =  134.900 ,
+							Price =  134 ,
 							ImageUrl = "https://m.xcite.com/media/catalog/product/cache/1/thumbnail/700x700/9df78eab33525d08d6e5fb8d27136e95/l/a/laptop-lenovo-grey-14-inch-v14-1.jpg"
 						},
 						new Laptop()
 						{
 							ProductName = "Microsoft Surface Pro 7",
 							Description ="Core i5 Ram 8GB SSD 128GB 12.3-inch Touchscreen Convertible Laptop",
-							Price =  319.900 ,
+							Price =  319,
 							ImageUrl ="https://m.xcite.com/media/catalog/product/cache/1/thumbnail/700x700/9df78eab33525d08d6e5fb8d27136e95/1/1/1112021-1warranty625219_1.jpg"
 						},
 						new Laptop()
 						{
 							ProductName = "HP Pavilion x360 14",
 							Description ="Intel Core i7 11th Gen. 16GB RAM 512GB SSD 14-inch Convertible Laptop",
-							Price =  319.900 ,
+							Price =  319 ,
 							ImageUrl= "https://m.xcite.com/media/catalog/product/cache/1/thumbnail/700x700/9df78eab33525d08d6e5fb8d27136e95/h/p/hp-pavilion-convertible-laptop-silver_5__1.jpg"
 						  }
 					});
